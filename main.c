8e3d09b56d2d113823d1104c25178dfdc594575e
anssobhi <anssobhi1996@gmail.com>
2021-02-12 11:44:44 +0200

    description

---
+++ main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    int grade;
+    do{
+    printf("enter your grade :");
+    scanf("%d", &grade);
+    }
+    while(grade<0||grade>100);
+    if (grade>=90&&grade<100){
+        printf("your grade is %d : A",grade);
+    }
+    else if (grade>=80&&grade<90){
+        printf("your grade is %d : B",grade);
+    }
+    else if (grade>=70&&grade<80){
+        printf("your grade is %d : C",grade);
+    }
+    else if (grade>=60&&grade<70){
+        printf("your grade is %d : D",grade);
+    }
+    else{
+        printf("your grade is %d : F",grade);
+    }*/
+    /*
+    int row,ast,astreq,sp,spreq;
+    for(row=1;row<=4;row++){
+            spreq = row-1;
+    for(sp=1;sp<=spreq;sp++){
+        printf(" ");
+    }
+        astreq = 9 - (row*2);
+        for(ast = 1;ast<=astreq;ast++){
+            printf("*");
+        }
+        printf("\n");
+    }*/
+    /*
+    int number;
+    printf("enter number ");
+    scanf("%d",&number);
+    (number%2==0)? printf("you enterd number %d : this is even ",number):printf("you enter number %d : this is odd",number);
+    */
+    /*
+    int num , count , sum = 0;
+    printf("please enter positive number :");
+    scanf("%d",&num);
+    for(count=1;count<=num;count++){
+        sum+=count;
+    }
+    printf("sum is %d",sum);*/
+    /*
+    int num,sum = 0;
+    do {
+        printf("please enter number");
+        scanf("%d",&num);
+        sum+=num;
+    }
+    while(num!=0);
+    printf("sum is %d \n",sum);
+    */
+    return 0;
+}
